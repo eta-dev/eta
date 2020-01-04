f0ca636be49fc77b2e5f1e7d7d4ded3a2e98204b
Ben Gubler <nebrelbug@gmail.com>
2020-01-04 15:55:30 -0700

    Updated tests with envs. Made envName optional on Compile

--- src/compile.ts
+++ src/compile.ts
@@ -1,7 +1,7 @@
 import CompileToString from './compile-string'
 import { Env } from './config'
 
-function Compile (str: string, tagOpen: string, tagClose: string, envName: string) {
+function Compile (str: string, tagOpen: string, tagClose: string, envName?: string) {
   var SqrlEnv = Env.get(envName || 'default')
   return new Function(SqrlEnv.varName, 'Sqrl', CompileToString(str, tagOpen, tagClose, SqrlEnv)) // eslint-disable-line no-new-func
 }
--- test/compile-string.spec.ts
+++ test/compile-string.spec.ts
@@ -1,4 +1,5 @@
 import { CompileToString } from '../src/index'
+import { Env } from '../src/config'
 var fs = require('fs'),
   path = require('path'),
   filePath = path.join(__dirname, 'templates/complex.sqrl')
@@ -10,49 +11,69 @@ const complexTemplate = fs.readFileSync(filePath, 'utf8')
  */
 describe('Compile to String test', () => {
   it('parses a simple template', () => {
-    var str = CompileToString('hi {{ hey }}', '{{', '}}')
+    var str = CompileToString('hi {{ hey }}', '{{', '}}', Env.get('default'))
     expect(str).toMatch(/[^]+/)
   })
 
   it('works with whitespace trimming', () => {
-    var str = CompileToString('hi {{-hey-}} {{_hi_}}', '{{', '}}')
+    var str = CompileToString('hi {{-hey-}} {{_hi_}}', '{{', '}}', Env.get('default'))
     expect(str).toMatch(/[^]+/)
   })
 
   it('works with filters', () => {
-    var str = CompileToString('hi {{ hey | stuff | stuff2 ("param1") }}', '{{', '}}')
+    var str = CompileToString(
+      'hi {{ hey | stuff | stuff2 ("param1") }}',
+      '{{',
+      '}}',
+      Env.get('default')
+    )
     expect(str).toMatch(/[^]+/)
   })
 
   it('works with helpers', () => {
-    var str = CompileToString('{{~each(x) => hi }} Hey {{#else }} oops {{/ each}}', '{{', '}}')
+    var str = CompileToString(
+      '{{~each(x) => hi }} Hey {{#else }} oops {{/ each}}',
+      '{{',
+      '}}',
+      Env.get('default')
+    )
     expect(str).toMatch(/[^]+/)
   })
 
   it('compiles complex template', () => {
-    var str = CompileToString(complexTemplate, '{{', '}}')
+    var str = CompileToString(complexTemplate, '{{', '}}', Env.get('default'))
     expect(str).toMatch(/[^]+/)
   })
 
   test('throws with bad filter syntax', () => {
     expect(() => {
-      CompileToString('{{~hi () hey | hi /}}', '{{', '}}')
+      CompileToString('{{~hi () hey | hi /}}', '{{', '}}', Env.get('default'))
     }).toThrow()
   })
 
   it('works with self-closing helpers', () => {
-    var str = CompileToString('{{~log ("hey") | hi /}}', '{{', '}}')
+    var str = CompileToString('{{~log ("hey") | hi /}}', '{{', '}}', Env.get('default'))
     expect(str).toMatch(/[^]+/)
   })
 
   it('works with helpers with results', () => {
-    var str = CompileToString('{{~log ("hey") => res, res2}}{{/log}}', '{{', '}}')
+    var str = CompileToString(
+      '{{~log ("hey") => res, res2}}{{/log}}',
+      '{{',
+      '}}',
+      Env.get('default')
+    )
     expect(str).toMatch(/[^]+/)
   })
 
   test("throws when helpers start and end don't match", () => {
     expect(() => {
-      CompileToString('{{~each(x) => hi }} Hey {{#else }} oops {{/ if}}', '{{', '}}')
+      CompileToString(
+        '{{~each(x) => hi }} Hey {{#else }} oops {{/ if}}',
+        '{{',
+        '}}',
+        Env.get('default')
+      )
     }).toThrow()
   })
 
