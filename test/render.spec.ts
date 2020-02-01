fc1966db6bbc42f1dd5c1f5fb5393e2bca17c0dc
Ben Gubler <nebrelbug@gmail.com>
2020-02-01 08:41:48 -0700

    Add async test

--- test/render.spec.ts
+++ test/render.spec.ts
@@ -1,5 +1,6 @@
 import Render from '../src/render'
 import Compile from '../src/compile'
+import { Config } from '../src/config'
 
 describe('Simple Render checks', () => {
   describe('Render works', () => {
@@ -13,6 +14,17 @@ describe('Simple Render checks', () => {
       expect(Render(Compile('Hi \n{{-it.name_}}  !', 'default'), { name: 'Ada Lovelace' })).toEqual(
         'Hi Ada Lovelace!'
       )
+    })
+    it('Rendering function works', async () => {
+      let template = 'Hello {{ await it.getName() }}!'
+      let getName = () => {
+        return new Promise((res, rej) => {
+          setTimeout(() => {
+            res('Ada')
+          }, 1000)
+        })
+      }
+      expect(await Render(template, { getName: getName }, { async: true })).toEqual('Hello Ada!')
     })
   })
 })
