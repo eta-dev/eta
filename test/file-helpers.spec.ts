d0eccb58ea73abdd295b522ee235b2f2329c8359
Ben Gubler <nebrelbug@gmail.com>
2020-02-17 10:13:23 -0700

    Add template inheritance tests

--- test/file-helpers.spec.ts
+++ test/file-helpers.spec.ts
@@ -1,4 +1,4 @@
-import { render, templates, compile } from '../src/index'
+import { render, templates, compile, renderFile } from '../src/index'
 import { defaultConfig } from '../src/config'
 
 var fs = require('fs'),
@@ -48,5 +48,52 @@ describe('include works', () => {
     expect(() => {
       render('{{~include("test-template", it)}} {{#block2}} {{/includeFile}}', { name: 'stuff' })
     }).toThrow()
+  })
+})
+
+describe('extends works', () => {
+  var layoutRes = `<h1>My layout</h1>
+<h2>
+Custom Title
+</h2>
+Ben's cool site
+<p> This is a cool layout by Ben</p>
+<strong>This is the content of the page
+</strong>`
+
+  it('"extendsFile" works', async () => {
+    var renderedTemplate = await renderFile(path.join(__dirname, 'templates/extends.sqrl'), {
+      name: 'Ben'
+    })
+
+    expect(renderedTemplate).toEqual(layoutRes)
+  })
+
+  var extendsTemplate = `{{~extends('layout1', it)}}
+This is the content of the page
+{{#title}}
+Custom Title
+{{#description}}
+{{it.name}}'s cool site
+{{/extends}}`
+
+  var layoutTemplate = `<h1>My layout</h1>
+<h2>
+{{~block('title')}}
+this is the default title
+{{/block}}
+</h2>
+{{~block('description')}}
+The default description
+{{/block}}
+<p> This is a cool layout by {{it.name}}</p>
+<strong>{{it.content}}</strong>`
+
+  it('"extends" works', async () => {
+    templates.define('layout1', compile(layoutTemplate))
+
+    var renderedTemplate = render(extendsTemplate, { name: 'Ben' })
+
+    expect(renderedTemplate).toEqual(layoutRes)
   })
 })
