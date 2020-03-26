1defecc497527179afbbe0806d30dd7063cac61d
Ben Gubler <nebrelbug@gmail.com>
2020-03-26 11:39:59 -0600

    Update dependencies, remove old parse drafts

--- dist/types/parse-old.d.ts
+++
@@ -1,8 +0,0 @@
-import { EtaConfig } from './config';
-export declare type TagType = 'r' | 'e' | 'i' | '';
-export interface TemplateObject {
-    t: TagType;
-    val: string;
-}
-export declare type AstObject = string | TemplateObject;
-export default function parse(str: string, env: EtaConfig): Array<AstObject>;
