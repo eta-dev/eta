3219e0a94e95bb8ddf2f0bf026ccce6c14d9e778
Ben Gubler <nebrelbug@gmail.com>
2020-09-01 14:21:50 -0600

    A few more tests

---
+++ test/deno/helpers.spec.ts
@@ -0,0 +1,39 @@
+import { assertEquals, assertThrows } from 'https://deno.land/std@0.67.0/testing/asserts.ts'
+import { render } from '../../deno_dist/mod.ts'
+
+// SHOULD TEST COMMON ETA USAGE PATTERNS HERE
+
+var eachTemplate = `
+The Daugherty's have 5 kids:
+<ul>
+<% it.kids.forEach(function(kid){ %>
+<li><%= kid %></li>
+<% }) %>
+</ul>`
+
+Deno.test('Loop over an array', () => {
+  var res = render(eachTemplate, { kids: ['Ben', 'Polly', 'Joel', 'Phronsie', 'Davie'] })
+
+  assertEquals(
+    res,
+    `
+The Daugherty's have 5 kids:
+<ul>
+<li>Ben</li>
+<li>Polly</li>
+<li>Joel</li>
+<li>Phronsie</li>
+<li>Davie</li>
+</ul>`
+  )
+})
+
+Deno.test('throws if helper "include" cannot find template', () => {
+  assertThrows(
+    () => {
+      render('<%~ E.include("missing-template", it) %>', {})
+    },
+    Error,
+    'Could not fetch template "missing-template"'
+  )
+})
