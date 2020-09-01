59877a7fc77c9a66c4f2628161acd1694c0a80b6
Ben Gubler <nebrelbug@gmail.com>
2020-09-01 14:04:05 -0600

    Move deno tests into subfolder

---
+++ test/deno/basic.spec.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from 'https://deno.land/std@0.67.0/testing/asserts.ts'
+import * as eta from '../../deno_dist/mod.ts'
+
+Deno.test('simple render', () => {
+  const t = `Hi <%=it.name%>`
+  assertEquals(eta.render(t, { name: 'Ben' }), 'Hi Ben')
+})
