98c60dcbe950486749ee80f9a29c0daff52e5663
Ben Gubler <nebrelbug@gmail.com>
2020-09-03 21:07:59 -0600

    Better JSDoc annotations, rearrange conditional

--- src/render.ts
+++ src/render.ts
@@ -24,6 +24,8 @@ function handleCache(template: string | TemplateFunction, options: EtaConfig): T
     templateFunc = compile(template, options)
   }
 
+  // Note that we don't have to check if it already exists in the cache;
+  // it would have returned earlier if it had
   if (options.cache && options.name) {
     options.templates.define(options.name, templateFunc)
   }
@@ -31,17 +33,46 @@ function handleCache(template: string | TemplateFunction, options: EtaConfig): T
   return templateFunc
 }
 
+/**
+ * Render a template
+ *
+ * If `template` is a string, Eta will compile it to a function and then call it with the provided data.
+ * If `template` is a template function, Eta will call it with the provided data.
+ *
+ * If `config.async` is `false`, Eta will return the rendered template.
+ *
+ * If `config.async` is `true` and there's a callback function, Eta will call the callback with `(err, renderedTemplate)`.
+ * If `config.async` is `true` and there's not a callback function, Eta will return a Promise that resolves to the rendered template
+ *
+ * If `config.cache` is `true` and `config` has a `name` or `filename` property, Eta will cache the template on the first render and use the cached template for all subsequent renders.
+ *
+ * @param template Template string or template function
+ * @param data Data to render the template with
+ * @param config Optional config options
+ * @param cb Callback function
+ */
+
 export default function render(
   template: string | TemplateFunction,
   data: object,
-  env?: PartialConfig,
+  config?: PartialConfig,
   cb?: CallbackFn
-) {
-  var options = getConfig(env || {})
+): string | Promise<string> | void {
+  var options = getConfig(config || {})
 
   if (options.async) {
     var result
-    if (!cb) {
+    if (cb) {
+      // If user passes callback
+      try {
+        // Note: if there is an error while rendering the template,
+        // It will bubble up and be caught here
+        var templateFn = handleCache(template, options)
+        templateFn(data, options, cb)
+      } catch (err) {
+        return cb(err)
+      }
+    } else {
       // No callback, try returning a promise
       if (typeof promiseImpl === 'function') {
         return new promiseImpl(function (resolve: Function, reject: Function) {
@@ -54,12 +85,6 @@ export default function render(
         })
       } else {
         throw EtaErr("Please provide a callback function, this env doesn't support Promises")
-      }
-    } else {
-      try {
-        handleCache(template, options)(data, options, cb)
-      } catch (err) {
-        return cb(err)
       }
     }
   } else {
