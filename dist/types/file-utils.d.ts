85ea87903917fdb03759523bdfe8cfbe8fff0c30
Ben Gubler <nebrelbug@gmail.com>
2020-09-07 21:35:16 -0600

    Rebuild after file-handling changes

--- dist/types/file-utils.d.ts
+++ dist/types/file-utils.d.ts
@@ -2,11 +2,15 @@ import { EtaConfig } from './config';
 /**
  * Get the absolute path to an included template
  *
- * If this is called with an absolute path (for example, starting with '/' or 'C:\') then Eta will return the filepath.
+ * If this is called with an absolute path (for example, starting with '/' or 'C:\')
+ * then Eta will attempt to resolve the absolute path within options.views. If it cannot,
+ * Eta will fallback to options.root or '/'
  *
  * If this is called with a relative path, Eta will:
  * - Look relative to the current template (if the current template has the `filename` property)
  * - Look inside each directory in options.views
+ *
+ * Note: if Eta is unable to find a template using path and options, it will throw an error.
  *
  * @param path    specified path
  * @param options compilation options
