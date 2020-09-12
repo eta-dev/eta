7f4c4b4f869162417f97f2d57baa857c39618cb8
Ben Gubler <nebrelbug@gmail.com>
2020-09-12 13:12:04 -0600

    Remove dist, deno_dist from master branch

--- deno_dist/file-methods.ts
+++
@@ -1,4 +0,0 @@
-export * as fs from "https://deno.land/std@0.66.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.66.0/path/mod.ts";
-
-export var readFileSync = Deno.readTextFileSync;
--- dist/types/file-methods.d.ts
+++
@@ -1,4 +0,0 @@
-export declare var fs: any;
-export declare var path: any;
-export declare var readFileSync: any;
-//# sourceMappingURL=file-methods.d.ts.map
