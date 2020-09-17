b3ada678237fd57f3258510302404116aa6a428e
Ben Gubler <nebrelbug@gmail.com>
2020-09-17 14:51:41 -0600

    Add tests for layouts without includeFile

--- test/layouts.spec.ts
+++ test/layouts.spec.ts
@@ -1,15 +1,15 @@
 import { path } from '../src/file-methods'
 /* global it, expect, describe */
 
-import { renderFile } from '../src/index'
+import { compile, render, renderFile, templates } from '../src/index'
 
 describe('Layout Tests', () => {
-  it('Layout works as expected', async () => {
+  it('Nested layouts work as expected', async () => {
     var res = await renderFile(
       'index.eta',
       { title: 'Cool  Title' },
       // Async can be true or false
-      { views: path.join(__dirname, 'templates'), async: false }
+      { views: path.join(__dirname, 'templates'), async: true }
     )
 
     expect(res).toEqual(`<!DOCTYPE html>
@@ -21,5 +21,21 @@ describe('Layout Tests', () => {
 This is the template body.
 </body>
 </html>`)
+  })
+
+  it('Layouts fall back to include if includeFile is undefined', async () => {
+    templates.define(
+      'my-layout',
+      compile(`###<%= it.title %>###,<%~ it.body %>`, { includeFile: undefined })
+    )
+
+    var res = await render(
+      `<% layout("my-layout") %>
+This is a layout`,
+      { title: 'Cool Title' },
+      { includeFile: undefined }
+    )
+
+    expect(res).toEqual('###Cool Title###,This is a layout')
   })
 })
