8dc90f3b92a68784decb4317aecd51e5c7ef5a6f
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-28 13:40:21 -0800

    fix: export from mod.ts

--- src/mod.deno.ts
+++ src/mod.deno.ts
@@ -5,13 +5,13 @@ import { config } from './config.ts'
 config.includeFile = includeFileHelper
 config.filepathCache = {}
 
-export { loadFile, renderFile, renderFile as __express } from './file-handlers.ts'
+export { loadFile, renderFile, renderFileAsync, renderFile as __express } from './file-handlers.ts'
 
 /* End file stuff */
 
 export { default as compileToString } from './compile-string.ts'
 export { default as compile } from './compile.ts'
 export { default as parse } from './parse.ts'
-export { default as render } from './render.ts'
+export { default as render, renderAsync } from './render.ts'
 export { templates } from './containers.ts'
 export { config, config as defaultConfig, getConfig, configure } from './config.ts'
