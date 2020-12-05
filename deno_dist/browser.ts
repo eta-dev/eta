4c1052dee5352dcb585bac5639695df5e2bbd897
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-12-05 13:59:44 -0800

    chore: rebuild deno_dist

--- deno_dist/browser.ts
+++ deno_dist/browser.ts
@@ -1,11 +1,6 @@
-export { default as compileToString } from "./compile-string.ts";
-export { default as compile } from "./compile.ts";
-export { default as parse } from "./parse.ts";
-export { default as render, renderAsync } from "./render.ts";
-export { templates } from "./containers.ts";
-export {
-  config,
-  config as defaultConfig,
-  configure,
-  getConfig,
-} from "./config.ts";
+export { default as compileToString } from './compile-string.ts'
+export { default as compile } from './compile.ts'
+export { default as parse } from './parse.ts'
+export { default as render, renderAsync } from './render.ts'
+export { templates } from './containers.ts'
+export { config, config as defaultConfig, getConfig, configure } from './config.ts'
--- deno_dist/mod.ts
+++ deno_dist/mod.ts
@@ -1,27 +1,17 @@
 /* Export file stuff */
-import { includeFileHelper } from "./file-helpers.ts";
-import { config } from "./config.ts";
+import { includeFileHelper } from './file-helpers.ts'
+import { config } from './config.ts'
 
-config.includeFile = includeFileHelper;
-config.filepathCache = {};
+config.includeFile = includeFileHelper
+config.filepathCache = {}
 
-export {
-  loadFile,
-  renderFile,
-  renderFile as __express,
-  renderFileAsync,
-} from "./file-handlers.ts";
+export { loadFile, renderFile, renderFileAsync, renderFile as __express } from './file-handlers.ts'
 
 /* End file stuff */
 
-export { default as compileToString } from "./compile-string.ts";
-export { default as compile } from "./compile.ts";
-export { default as parse } from "./parse.ts";
-export { default as render, renderAsync } from "./render.ts";
-export { templates } from "./containers.ts";
-export {
-  config,
-  config as defaultConfig,
-  configure,
-  getConfig,
-} from "./config.ts";
+export { default as compileToString } from './compile-string.ts'
+export { default as compile } from './compile.ts'
+export { default as parse } from './parse.ts'
+export { default as render, renderAsync } from './render.ts'
+export { templates } from './containers.ts'
+export { config, config as defaultConfig, getConfig, configure } from './config.ts'
