18bd212ddadf2229f5cac5e7177fb766b9465f1d
v1rtl <pilll.pl22@gmail.com>
2021-05-25 20:52:00 +0300

    chore: remove deno_dist

--- deno_dist/file-methods.ts
+++
@@ -1,4 +0,0 @@
-export * as fs from "https://deno.land/std@0.97.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.97.0/path/mod.ts";
-
-export const readFileSync = Deno.readTextFileSync;
