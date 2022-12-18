6d2c1ac4f65ecec982fb688377360277e3404660
Ben Gubler <nebrelbug@gmail.com>
2022-12-17 20:23:11 -0700

    Rebuild

--- deno_dist/file-methods.ts
+++ deno_dist/file-methods.ts
@@ -1,4 +1,4 @@
-export { existsSync } from "https://deno.land/std@0.97.0/fs/exists.ts";
-export * as path from "https://deno.land/std@0.97.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.97.0/fs/exists.ts"
+export * as path from "https://deno.land/std@0.97.0/path/mod.ts"
 
-export const readFileSync = Deno.readTextFileSync;
+export const readFileSync = Deno.readTextFileSync
