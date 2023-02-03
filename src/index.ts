b7548d2a5ebfe81124e9b82d683b2e8901941d75
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-02-03 18:29:34 +0545

    fix: export EtaConfig

--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,4 @@ export { default as compile } from "./compile.js";
 export { default as parse } from "./parse.js";
 export { default as render, renderAsync } from "./render.js";
 export { templates } from "./containers.js";
-export { config, config as defaultConfig, getConfig, configure } from "./config.js";
+export { config, config as defaultConfig, getConfig, configure,  EtaConfig } from "./config.js";
