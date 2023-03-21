9e996a6d35d83935621dd4b44b0a133b4d8c7c9c
Gün <Gn>
2023-03-21 10:27:38 +0100

    use export type

--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,5 @@ export { default as compile } from "./compile.js";
 export { default as parse } from "./parse.js";
 export { default as render, renderAsync } from "./render.js";
 export { templates } from "./containers.js";
-export { EtaConfig, config, config as defaultConfig, getConfig, configure } from "./config.js";
+export { config, config as defaultConfig, getConfig, configure } from "./config.js";
+export type EtaConfig = typeof config;
