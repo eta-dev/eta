bcb9d2d8946fc3c27b7cb2d77802856e06f99f6d
diego tonini <diego.tonini93@gmail.com>
2024-03-02 01:00:54 +0100

    feat: use custom error for compile() api

--- test/err.spec.ts
+++ test/err.spec.ts
@@ -6,9 +6,23 @@ import { Eta, EtaParseError, EtaRuntimeErr } from "../src/index";
 describe("ParseErr", () => {
   const eta = new Eta();
 
-  it("error while parsing", () => {
+  it("error while parsing - renderString", () => {
     try {
       eta.renderString("template <%", {});
+    } catch (ex) {
+      expect(ex).toBeInstanceOf(EtaParseError);
+      expect((ex as EtaParseError).name).toBe("EtaParser Error");
+      expect((ex as EtaParseError).message).toBe(`unclosed tag at line 1 col 10:
+
+  template <%
+           ^`);
+      expect(ex instanceof Error).toBe(true);
+    }
+  });
+
+  it("error while parsing - compile", () => {
+    try {
+      eta.compile("template <%");
     } catch (ex) {
       expect(ex).toBeInstanceOf(EtaParseError);
       expect((ex as EtaParseError).name).toBe("EtaParser Error");
