f468e204da032613344286658fb9cf65a2590445
pasha-bolokhov <pasha.a.bolokhov@gmail.com>
2024-08-07 14:54:28 -0700

    export "Options" and "EtaConfig" types in JSR (#295) (#297)

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { Eta as EtaCore } from "./core.ts";
 import { readFile, resolvePath } from "./file-handling.ts";
 export {
   EtaError,
-  EtaParseError,
-  EtaRuntimeError,
   EtaFileResolutionError,
   EtaNameResolutionError,
+  EtaParseError,
+  EtaRuntimeError,
 } from "./err.ts";
+export { type EtaConfig, type Options } from "./config.ts";
 
 export class Eta extends EtaCore {
   readFile = readFile;
